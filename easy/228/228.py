c71c1a9503c2d82115cf869b156c7f4a45e54f91
christianly326 <christianyacapin@gmail.com>
2024-09-01 11:46:25 +0100

    Added Question 228 python solution

---
+++ easy/228/228.py
@@ -0,0 +1,14 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        output = []
+        i = 0 
+        while i < len(nums):
+            start = nums[i]
+            while i < len(nums) - 1 and nums[i] + 1 == nums[i + 1]:
+                i += 1
+            if start != nums[i]:
+                output.append(f'{start}->{nums[i]}')
+            else:
+                output.append(f'{start}')
+            i += 1
+        return output
